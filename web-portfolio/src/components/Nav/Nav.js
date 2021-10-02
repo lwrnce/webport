bf410a5ff4eaa4503c11259ff6213d4bddaa899f
Lawrence Yuen <lwrnceyuen@gmail.com>
2021-10-02 14:42:03 -0700

    create simple navbar

---
+++ web-portfolio/src/components/Nav/Nav.js
@@ -0,0 +1,14 @@
+
+function Nav() {
+  return (
+      <div class="topnav">
+          <a class="active" href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#projects">Projects</a>
+          <a href="#experience">Experience</a>
+          <a href="#contact">Contact</a>
+      </div>
+  );
+}
+
+export default Nav;
